2c39a72fd8b322c3f9a2b59abf7cde7211d7514f
omagari.tomohisa <ooma0301@gmail.com>
2014-01-05 08:24:28 +0900

    change default

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
+execute "yum update" do
+    command <<-EOH
+    rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    yum update -y
+    EOH
+end
+
 %w{curl wget gcc make vim}.each do |pkg|
   package pkg do
     action [:install, :upgrade]
